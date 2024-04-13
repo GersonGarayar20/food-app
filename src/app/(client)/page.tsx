048d51eebca440a10666df03c04349091b654f66
estebans salas sulca <esalassulca@gmail.com>
2024-04-13 11:29:33 -0500

    Ignorar package-lock.json en futuros commits

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -2,19 +2,36 @@ import ProductCard from "@/components/product-card";
 import { Input } from "@/components/ui/input";
 import { products } from "@/data/products";
 import React from "react";
+import { FilterProducts } from "./components/FilterProducts";
+import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import { useQuery } from "react-query";
+import { getProducts } from "@/lib/products";
 
 export default function HomePage() {
+
+
+const products = useQuery('products', getProducts)
+
   return (
-    <main>
-      <h1>hola mundo</h1>
-      <Input placeholder="buscador" />
-      {/* categorias */}
-      <div className="flex gap-2 py-2">
-        <div>pizza</div>
-        <div>hamburger</div>
+    <main className=" min-h-screen px-4">
+      <h1 className="text-2xl font-bold my-2">Bravazo</h1>
+      <h2 className="mb-8">Lo mejor en un solo lugar!!</h2>
+      <div  className="flex gap-4 h-12 mb-6">
+        <Input placeholder="buscador" className="h-full rounded-lg" />
+        <FilterProducts/>
+      </div>
+      <div>
+        
+      </div>
+      <h2 className="mb-4">Categorias</h2>
+      <div className="flex gap-2 mb-4">
+        <Button className="rounded-2xl">All</Button>
+        <Button className="rounded-2xl">Pizza</Button>
+        <Button className="rounded-2xl">Burger</Button>
       </div>
       <section>
-        <h2>Productos</h2>
+        <h2 className="mb-4">Productos</h2>
         <div className="grid md:grid-cols-4 grid-cols-2 gap-4">
           {products.map(({ category_id, name, image, price }) => (
             <ProductCard
@@ -27,6 +44,7 @@ export default function HomePage() {
           ))}
         </div>
       </section>
+      <Navbar/>
     </main>
   );
 }
