3ee76893bb940277efb41b7cabca2c9715903468
Jose <dstryr77@gmail.com>
2024-04-13 09:36:32 -0500

    update handle errors

---
+++ src/lib/api/Interceptor/interceptor.ts
@@ -0,0 +1,11 @@
+import { api } from "../axios";
+
+
+export function AxiosInterceptor() {
+    api.interceptors.response.use(function(response) {
+        return response.data
+    },function (error) {   
+        return error.response.data
+    })
+
+}
