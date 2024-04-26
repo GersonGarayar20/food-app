1b948f24babf859dd19a39e93f38f11f46e98170
Jose <dstryr77@gmail.com>
2024-04-26 18:05:35 -0500

    add link cart to homepage

--- src/app/(client)/page.client.tsx
+++ src/app/(client)/page.client.tsx
@@ -2,12 +2,15 @@
 import { FilterProducts } from "./components/FilterProducts";
 import ProductList from "./components/ProductList";
 import CategoryList from "./components/CategoryList";
-import {Bell,BellIcons} from "@/components/icons/Bell";
 import Link from "next/link";
 import {  Heart } from "lucide-react";
+import { CartNavigate } from "@/components/icons/ArrowBack";
+import { useStore } from "zustand";
+import { useCartStore } from "@/store/cart";
 
 export default function HomePage({user}:{user:any}) {
 
+  const stateCart=useStore(useCartStore,useCartStore.getState)
   
   return (
     <main className=" min-h-screen p-4">
@@ -15,11 +18,14 @@ export default function HomePage({user}:{user:any}) {
         <div>
           <h2 className="text-3xl">Bravazo</h2>
         </div>
-        <div className="flex gap-4 ">
+        <div className="flex gap-4 items-center justify-center">
           <Link href={"/favorites"}>
             <Heart className="w-8 h-8" />
           </Link>
-
+          <div className="relative bg-white dark:bg-transparent rounded-3xl p-1 lg:top-0 lg:right-0" >
+            <div className="rounded-full bg-orange-600 text-white absolute w-6 -top-1 -right-1 z-20 text-center" ><p className="" id="count">{stateCart.count()}</p></div>
+            <CartNavigate className='text-dark dark:text-white'/>
+          </div>
           <Link href={"/profile"}>
             <img
               src={user?.image?user.image:"https://randomuser.me/api/portraits/men/62.jpg"}
