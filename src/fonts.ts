46534f18ae0f83a089485e2267f8cc782d9450af
gerson <garayargerson20@gmail.com>
2024-05-16 21:49:38 -0500

    agregando fuentes

---
+++ src/fonts.ts
@@ -0,0 +1,12 @@
+export const pollerOne = Poller_One({
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
+
+export const roboto = Roboto({
+  weight: ["100", "300", "400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
