041d8503f7b4f8aa6f380f062c26e22081f6fe06
gerson <garayargerson20@gmail.com>
2024-05-17 14:24:42 -0500

    solucion de error

--- src/app/admin/categories/create/page.tsx
+++ src/app/admin/categories/create/page.tsx
@@ -4,11 +4,12 @@ import { authOptions } from "@/lib/auth";
 
 export default async function page() {
   const session = await getServerSession(authOptions);
+  if (!session) return <div>no has iniciado sesion</div>;
 
   return (
-    <>
+    <div>
       <h1 className="text-2xl">Crear una categoria</h1>
       <CategoriesCreateForm token={session.user.accessToken} />
-    </>
+    </div>
   );
 }
