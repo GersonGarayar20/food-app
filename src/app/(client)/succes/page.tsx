42b8d568a7ce04c8264bb91418783a2838062d19
estebans salas sulca <esalassulca@gmail.com>
2024-05-17 13:49:37 -0500

    remover window de la pagina succes

--- src/app/(client)/succes/page.tsx
+++ src/app/(client)/succes/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { IsServer } from "@/util/VerifyWindow";
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
@@ -12,14 +11,14 @@ import {
 } from "@/components/ui/card";
 import Link from "next/link";
 import Confetti from "react-confetti";
-
+/* 
 interface WindowSize {
   width: number;
   height: number;
 }
-
+ */
 const Success: React.FC = () => {
-  const [windowSize, setWindowSize] = useState<WindowSize>(() => {
+/*   const [windowSize, setWindowSize] = useState<WindowSize>(() => {
     if (IsServer()) return { width: 0, height: 0, }
     return {
       width:window.innerWidth,
@@ -43,10 +42,10 @@ useEffect(() => {
     };
   }
 }, []);
-
+ */
 return (
   <div className="p-8">
-    <Confetti width={windowSize.width} height={windowSize.height} />
+    {/* <Confetti width={windowSize.width} height={windowSize.height} /> */}
     <div className="mb-16">
       <h1 className="text-center font-bold text-7xl my-8">Bravazo</h1>
     </div>
